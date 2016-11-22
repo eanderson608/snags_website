e3b85d590b86ca769b9bccef570e93aad9091c37
eanderson608 <eanderson608@gmail.com>
2016-11-21 19:56:05 -0600

    seperated index.js from script.js

---
+++ index.js
@@ -0,0 +1,15 @@
+formatGoogleCalendar.init({
+	calendarUrl: 'https://www.googleapis.com/calendar/v3/calendars/iuu19aualns1ff3n1lr4j1116c@group.calendar.google.com/events?key=AIzaSyAq5NdUw04tb1D6Z0yie32s1Hu8c2s_-2Y',
+	past: false,
+	upcoming: true,
+	sameDayTimes: true,
+	dayNames: true,
+	pastTopN: 0,
+	upcomingTopN: 10,
+	itemsTagName: 'div',
+	upcomingSelector: '#events-upcoming',
+	pastSelector: '#events-past',
+	upcomingHeading: '<h2>Upcoming events</h2>',
+	pastHeading: '<h2>Past events</h2>',
+	format: ['*summary*', '*date*', '<br>', '*description*']
+});
